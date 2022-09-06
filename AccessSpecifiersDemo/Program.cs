88fdfd69d5064f8176a4bbd51121aabc3d430e82
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-06 09:41:08 +0530

    Add project files.

---
+++ AccessSpecifiersDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpecifiersLibrary;
+namespace AccessSpecifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Empid = 101;
+            emp.
+        }
+    }
+}
---
+++ SpecifiersLibrary/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecifiersLibrary
+{
+    //public----->If the assembly(class library) code  is to be used across another(console app) assembly then the access specifiers to use the class must be public (classes and methods of classes)
+
+    //private---- will only be accessible to that particular class only.
+    //protected---allowed only for a child class
+    //internal---- accessible only to the containing assembly
+    //protected internal---allowed only for a child class and 
+    //accessible only to the containing assembly
+    public class Employee
+    {
+        public int Empid { get; set; }
+        private string Password { get; set; }
+        protected int Salary { get; set; }
+        internal string NickName { get; set; }
+        protected internal string City { get; set; }
+    }
+
+    public class Manager:Employee
+    {
+        private void UseOfProtected()
+        {
+            base.Salary = 1000;
+            base.City = "Pune";
+        }
+   }
+
+    class Supervisor
+    {
+
+        public void MethodToInternalCall()
+        {
+            Employee emp = new Employee();
+            emp.
+        
+        
+        }
+    }
+}
