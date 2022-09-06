47b1f6ea9c5a75234f1aca4c87b5bf9b17230e48
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-06 12:44:04 +0530

    extension demo

--- AccessSpecifiersDemo/Program.cs
+++ AccessSpecifiersDemo/Program.cs
@@ -16,15 +16,47 @@ namespace AccessSpecifiersDemo
             //ATMMachine machine = new ATMMachine();
             //machine.ShowMenu();
 
-            Person p = new Person();
-            p[0] = "CH";
-            Console.WriteLine(p[0]);
-            Console.WriteLine(p[1]);
-            Console.WriteLine(p[2]);
-            Console.WriteLine(p[3]);
-            Console.WriteLine(p[4]);
+            //Person p = new Person();
+            //p[0] = "CH";
+            //Console.WriteLine(p[0]);
+            //Console.WriteLine(p[1]);
+            //Console.WriteLine(p[2]);
+            //Console.WriteLine(p[3]);
+            //Console.WriteLine(p[4]);
+            //ExtensionMethodsDemo.newmethod();
+            //Person p1 = new Person();
+            //p1.method1();
+            
+            string s="hello";
+
+            s.LengthOfString();
+
+            int i = 10;
+            int areaOFSq=i.calculateArea();
+            Console.WriteLine(areaOFSq);
+
+            int j = 20;
+            float area = j.calculateArea(15);
+            Console.WriteLine(area);
 
 
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(101);
+            list.Add(12);
+            list.Add(31); 
+            list.Add(33); 
+            list.Add(43);
+            list.Add(19);
+            list.Add(5);
+
+            double avgOfNos=list.Average();
+            Console.WriteLine("Average=" + avgOfNos);
+            int a=list.ElementAt(2);
+            Console.WriteLine("Elemnet at=" + a);
+
+            int[] arr = new int[] { 3, 13, 4, 322, 22 };
+            
 
             Console.Read();
         }
