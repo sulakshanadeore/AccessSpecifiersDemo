3e661e83c3e5470708138c46204f1ec5c1937350
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-06 12:06:09 +0530

    indexers

--- AccessSpecifiersDemo/Program.cs
+++ AccessSpecifiersDemo/Program.cs
@@ -13,8 +13,19 @@ namespace AccessSpecifiersDemo
             //Employee emp = new Employee();
             //emp.Empid = 101;
             //emp.Password = "abc@123";
-            ATMMachine machine = new ATMMachine();
-            machine.ShowMenu();
+            //ATMMachine machine = new ATMMachine();
+            //machine.ShowMenu();
+
+            Person p = new Person();
+            p[0] = "CH";
+            Console.WriteLine(p[0]);
+            Console.WriteLine(p[1]);
+            Console.WriteLine(p[2]);
+            Console.WriteLine(p[3]);
+            Console.WriteLine(p[4]);
+
+
+
             Console.Read();
         }
     }
